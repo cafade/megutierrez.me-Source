0dd7f0398b91c6a526d5cf46c6fe967d9313116e
learnin <cafaders@gmail.com>
2021-04-14 15:18:42 -0500

    Move event listener function back to index

--- scripts/functions.js
+++ scripts/functions.js
@@ -32,13 +32,3 @@ if (userLanguage == "es") {
 } else {
     appendContent("en")
 }
-
-
-if ('addEventListener' in window) {
-
-    window.addEventListener('load', function() { 
-        document.body.className = document.body.className.replace(/\bis-preload\b/, ''); 
-    });
-    document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
-
-}
